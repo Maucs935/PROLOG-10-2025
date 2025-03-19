8bf73e3f260347b463cff885be9b6d08f252c593
Mauricio Contreras Garcia <mauriciocontras43@gmail.com>
2025-03-19 08:53:06 -0600

    Explicacion Potencia Con sumas.

--- Potencia y Division/PotenciaConSumas.lisp
+++ Potencia y Division/PotenciaConSumas.lisp
@@ -1,5 +1,6 @@
-# Función Recursiva con Sumas para Calcular Potencias
+# Función Recursiva de Potencia con Sumas
 
+## Código
 ```lisp
 (defun potencia (base exponente)
   (if (= exponente 0)
@@ -7,3 +8,16 @@
       (+ base (potencia base (- exponente 1)))))
 
 (potencia 2 5)
+
+##Explicación
+potencia: Función recursiva que calcula la potencia de un número utilizando sumas en lugar de multiplicaciones.
+
+Si exponente es 0, retorna 1 como caso base.
+En cada llamada recursiva, suma base y reduce exponente en 1 hasta alcanzar el caso base.
+Ejecución:
+
+Se llama a potencia con base = 2 y exponente = 5, sumando repetidamente 2 hasta completar la cantidad de veces indicada por exponente.
+
+##Ejemplo de Salida
+11
+
